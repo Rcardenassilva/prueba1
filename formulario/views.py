a36a94545c290612d1c6b4b01c11f7cb455e06bb
Rcardenassilva <rcardenassilva@gmail.com>
2024-05-15 16:58:25 -0400

    first commit

--- formulario/views.py
+++ formulario/views.py
@@ -4,27 +4,29 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
 from django.urls import reverse_lazy
-# Create your views here.
-# TODO: crear funcionalidad de poder registrarse y el login internamente dentro de este metodo
+from django.core.exceptions import ObjectDoesNotExist
+ 
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        
-        if form.is_valid():
-            user = form.save()
-            # Asignar al usuario a un group
-            group = Group.objects.get(name='Operarios')
-            user.groups.add(group)
-            messages.success(request, 'Registro exitoso. Bienvenido!')
-            return redirect('home')
-        else:
-            for field in form.errors:
-                form[field].field.widget.attrs['class'] += 'is-invalid'
-            messages.error(request, 'Por favor corrija los errores en el formulario')
+       form = UserCreationForm(request.POST)
+       if form.is_valid():
+           with transaction.atomic():
+               user = form.save()
+               try:
+                    group = Group.objects.get(name='Operarios')
+                    user.groups.add(group)
+               except ObjectDoesNotExist:
+                   messages.warning(request, 'El grupo no existe')
+               return redirect('home')
+       else:
+           messages.error('corriga los errores de formulario')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+                   
 
 class CustomLoginView(LoginView):
     template_name = 'accounts/login.html'
@@ -32,12 +34,10 @@ class CustomLoginView(LoginView):
     
     def get_success_url(self):
         messages.success(self.request, 'Inicio de sesión exitoso, Bienvenido(a)!')
-        return reverse_lazy('home')
+        return reverse_lazy('formulario:home')
     
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('login')
     
-@login_required
 def home(request):
     return render(request, 'accounts/home.html')
-    
